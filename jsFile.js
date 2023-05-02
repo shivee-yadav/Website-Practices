714d8bc994bde464adade64a8dbe8613e949005a
shivee-yadav <shiveeyadav089@gmail.com>
2023-05-03 04:50:11 +0530

    new changes

--- jsFile.js
+++ jsFile.js
@@ -1,38 +1,11 @@
 //Document object model is used when we need to modify html by using js
 
 const taskContainer = document.querySelector(".task__container");
-const globalStore=[];//array of objects
+const globalStore = [];//array of objects
 
 console.log(taskContainer);
 
-const loadInitialCardData = () => {
-    //local storage to get tasky card data
-    const getCardData= localStorage.getItem("tasky");
-
-    //convert to normal object
-    const {cards} =JSON.parse(getCardData);
-
-
-    //loop over those array of task object to create HTML card,inject it in to DOM
-    cards.map((cardObject) => {
-        taskContainer.insertAdjacentHTML("beforeend",newCard(cardObject));
-    }
-    
-    )
-
-    //update our globalStore
-};
-
-const saveChanges = () => {
-    const taskData = {
-        id: `${Date.now()}`,  //$ is used because we're storing something dynamic
-        imageUrl: document.getElementById("imageurl").value,
-        taskTitle: document.getElementById("tasktitle").value,
-        taskType: document.getElementById("tasktype").value,
-        taskDescription: document.getElementById("taskdescription").value
-    };
-
-    const newCard = `
+const generateNewCard = (taskData) => `
    
         <div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id}>
             <div class="card">
@@ -52,10 +25,40 @@ const saveChanges = () => {
    
 `;
 
-    taskContainer.insertAdjacentHTML("beforeend", newCard);
+const loadInitialCardData = () => {
+    //local storage to get tasky card data
+    const getCardData = localStorage.getItem("tasky");
+
+    //convert to normal object
+    const {cards} = JSON.parse(getCardData);
+
+
+    //loop over those array of task object to create HTML card,inject it in to DOM
+    cards.map((cardObject) => {
+        taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
+
+        //update our globalStore
+        globalStore.push(cardObject);
+    }
+
+    )
+
+};
+
+const saveChanges = () => {
+    const taskData = {
+        id: `${Date.now()}`,  //$ is used because we're storing something dynamic
+        imageUrl: document.getElementById("imageurl").value,
+        taskTitle: document.getElementById("tasktitle").value,
+        taskType: document.getElementById("tasktype").value,
+        taskDescription: document.getElementById("taskdescription").value
+    };
+
+   
+    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
 
     globalStore.push(taskData);
-    localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));// stringify converts object of objects to array of objects,cards is a random key and globalStore is the value
+    localStorage.setItem("tasky",JSON.stringify({ cards: globalStore }));// stringify converts object of objects to array of objects,cards is a random key and globalStore is the value
 
 
 
